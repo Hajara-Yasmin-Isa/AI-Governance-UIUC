0604b4d3e542a800a3e6b0eca45bfb942f1e5c7d
isa <elhamisa04@gmail.com>
2025-06-17 17:54:55 -0500

    All pages and format completed

---
+++ script/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("learnMoreBtn");
+    if (btn) {
+      btn.addEventListener("click", () => {
+        alert("Check out our About page to learn more about what we do!");
+      });
+    }
+  });
+
+  
+  
